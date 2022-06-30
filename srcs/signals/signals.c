433390f5a810fbdbd7aede85b8a8193be364a1bb
Matey Nikolov <mateynnikolov@gmail.com>
2022-06-30 13:05:18 +0200

    updated

--- srcs/signals/signals.c
+++ srcs/signals/signals.c
@@ -14,17 +14,16 @@
 
 void    sigint_handler(int sig)
 {
-    (void)sig;
-    printf("\n");
-    rl_on_new_line();
-    rl_replace_line("", 0);
-    rl_redisplay();
-}
-
-void    sigquit_handler(int sig)
-{
-    (void)sig;
-    printf("Quit: %d\n", sig);
+    if (sig == SIGINT)
+    {
+        rl_on_new_line();
+        rl_redisplay();
+        printf(" \n");
+        rl_on_new_line();
+        rl_replace_line("", 1);
+        rl_redisplay();
+        g_ms.exit = 1;
+    }
 }
 
 void    init_signals(void)
