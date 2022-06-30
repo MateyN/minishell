9e5edaf3bda3a88a59b6c55979f27fa4bdef56af
Jud42 <judiismals@gmail.com>
2022-06-30 11:53:40 +0200

    draw_parse

---
+++ srcs/jud/init_struct.c
@@ -0,0 +1,29 @@
+#include "minishell.h"
+
+int     check_pipe(char **tab)
+{
+	//pipe ne doit pas Ãtre a l'intÃrieur des quotes
+	int     i;
+	int	j;
+	
+	j = -1;
+	i = 0;
+ 	while (tab[++j])
+		if (tab[j][0] == '|')
+			i++;
+	return (i);
+}
+
+void 	init_struct(t_lst *li, char **tab)
+{
+	int	i;
+
+	i = -1;
+	li->tab = tab;
+	li->nb_arg = 0;
+	while (tab[++i])
+		li->nb_arg++;
+	li->pipe = check_pipe(tab);
+	li->head = NULL;
+	tab = NULL;
+}
